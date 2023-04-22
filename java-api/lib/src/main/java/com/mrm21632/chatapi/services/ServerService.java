81705c6c7c4766e6ed2533bcbb2e5d392864d090
Joshua Morrison <MrM21632@users.noreply.github.com>
2023-04-22 13:24:05 -0400

    Handling POST calls for servers

--- java-api/lib/src/main/java/com/mrm21632/chatapi/services/ServerService.java
+++ java-api/lib/src/main/java/com/mrm21632/chatapi/services/ServerService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mrm21632.chatapi.models.Server;
 import com.mrm21632.chatapi.models.requests.ServerPostRequestBody;
@@ -13,6 +16,7 @@ import spark.Request;
 import spark.Response;
 
 public class ServerService {
+    private static final Logger logger = LoggerFactory.getLogger(ServerService.class.getName());
     private static final Session session = HibernateUtils.getSessionFactory().openSession();
 
     public static List<Server> getAll(Request req, Response res) {
@@ -29,6 +33,19 @@ public class ServerService {
     }
 
     public static Server add(ServerPostRequestBody body) {
-        return null;
+        Server server = new Server()
+            .setServerName(body.getServerName())
+            .setServerid(UUID.randomUUID());
+
+        Transaction tx = session.beginTransaction();
+        try {
+            session.merge(server);
+            tx.commit();
+            return server;
+        } catch (Exception e) {
+            logger.error("Exception occurred attempting to save new server to datastore", e);
+            tx.rollback();
+            return null;
+        }
     }
 }
